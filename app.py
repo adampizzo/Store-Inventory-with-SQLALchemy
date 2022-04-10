0ddf6442eefa5165fc68da02bb1a39ab7818f837
adampizzo <Adampizzo@icloud.com>
2022-04-10 00:51:57 -0400

    Updated Add_CSV logic
    changed from .one_or_none to seeing if items are in a query list.
    
    created prototype add_item_to_inventory list, as well as clean_qty function.
    
    add placeholder functions:
    replace_existing_item and item_in_inventory.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+from ast import In
+from datetime import datetime, timezone
+from tkinter import Y
 from models import (Base, session, Inventory, engine)
 import datetime
 import csv
@@ -19,8 +21,8 @@ def clean_price(price_str):
     except ValueError:
         input('''
             \n****** PRICE ERROR ******
-            \rThe price should be a number without a current symbol.
-            \rEx: 10.99
+            \rThe price should be a number with a current symbol.
+            \rEx: $10.99
             \rPress enter to try again.
             \r*************************''')
         return
@@ -72,32 +74,48 @@ def clean_id(id_str, id_options):
         return item_id
 
 
-def compare_items(db_item, other_item):
-    if db_item == None:
-        name = other_item[0]
-        price = clean_price(other_item[1])
-        qty = int(other_item[2])
-        date = clean_date(other_item[3])
-        new_item = Inventory(product_name=name, product_price=price, product_qty=qty, date_updated=date)
-        session.add(new_item)
-        session.commit()
-        logging.info(f'New item added to database: {new_item}.')
-    elif db_item.date_updated > clean_date(other_item[3]):
-        logging.info(
-            f'"{db_item.product_name}" already exists in the database and a more recent item exists in the database. Discarding item.')
-    elif db_item.date_updated < clean_date(other_item[3]):
-        logging.info(f'***** REPLACING Item *****')
-        logging.info(f'\tOld Item: Name: {db_item.product_name}, ID: {db_item.product_id}, Price: {db_item.product_price}, Quantity: {db_item.product_qty}, Date Updated: {db_item.date_updated}.')
-        logging.info(f'\tNew Item: Name: {other_item[0]}, ID: {db_item.product_id}, Price: {clean_price(other_item[1])}, Quantity: {int(other_item[2])}, Date Updated: {clean_date(other_item[3])}.')
-        db_item.product_name = other_item[0]
-        db_item.product_price = clean_price(other_item[1])
-        db_item.product_qty = int(other_item[2])
-        db_item.date_updated = clean_date(other_item[3])
-        session.commit()
-        logging.info(
-            f'{db_item.product_name} successfully updated.')
+def clean_qty(qty_str):
+    try:
+        item_qty = int(qty_str)
+    except ValueError:
+        input('''
+            \n****** QUANTITY ERROR ******
+            \rThe quantity should be a number.
+            \rEx: 15
+            \rPress enter to try again.
+            \r****************************
+            ''')
+        return
     else:
-        logging.info(f'"{db_item.product_name}" already exists in the database and no other entries detected. Discarding item.')
+        return item_qty
+
+
+# def compare_items(db_item, other_item):
+#     if db_item == None:
+#         name = other_item[0]
+#         price = clean_price(other_item[1])
+#         qty = int(other_item[2])
+#         date = clean_date(other_item[3])
+#         new_item = Inventory(product_name=name, product_price=price, product_qty=qty, date_updated=date)
+#         session.add(new_item)
+#         session.commit()
+#         logging.info(f'New item added to database: {new_item}.')
+#     elif db_item.date_updated > clean_date(other_item[3]):
+#         logging.info(
+#             f'"{db_item.product_name}" already exists in the database and a more recent item exists in the database. Discarding item.')
+#     elif db_item.date_updated < clean_date(other_item[3]):
+#         logging.info(f'***** REPLACING Item *****')
+#         logging.info(f'\tOld Item: Name: {db_item.product_name}, ID: {db_item.product_id}, Price: {db_item.product_price}, Quantity: {db_item.product_qty}, Date Updated: {db_item.date_updated}.')
+#         logging.info(f'\tNew Item: Name: {other_item[0]}, ID: {db_item.product_id}, Price: {clean_price(other_item[1])}, Quantity: {int(other_item[2])}, Date Updated: {clean_date(other_item[3])}.')
+#         db_item.product_name = other_item[0]
+#         db_item.product_price = clean_price(other_item[1])
+#         db_item.product_qty = int(other_item[2])
+#         db_item.date_updated = clean_date(other_item[3])
+#         session.commit()
+#         logging.info(
+#             f'{db_item.product_name} successfully updated.')
+#     else:
+#         logging.info(f'"{db_item.product_name}" already exists in the database and no other entries detected. Discarding item.')
 
 
 def display_inventory():
@@ -128,7 +146,36 @@ def search_inventory_by_id():
     input('Press enter to return to the main menu...')
 
 
-def add_product_to_inventory():
+def add_item_to_inventory():
+    # ask for name
+    name = input('Name of Item: ')
+    # ask for qty
+    qty_error = True
+    while qty_error:
+        qty = input('Quantity of Item Available: ')
+        qty = clean_qty(qty)
+        if type(qty) == int:
+            qty_error = False
+    # ask for price
+    price_error = True
+    while price_error:
+        price = input('Price of Each Item (Ex: $5.99): ')
+        price = clean_price(price)
+        if type(price) == int:
+            price_error = False
+    # datetime.now
+    # derived from Tim Richardson at https://stackoverflow.com/questions/15940280/how-to-get-utc-time-in-python
+    current_time = datetime.datetime.now(timezone.utc).strftime('%Y-%d-%m')
+    
+    new_item = Inventory(product_name=name, product_qty=qty, product_price=price, date_updated=current_time)
+    session.add(new_item)
+    session.commit()
+    print(f'\n{new_item.product_name} has been successfully added to the database!')
+
+def replace_existing_item():
+    pass
+
+def item_in_invetory():
     pass
 
 
@@ -141,9 +188,43 @@ def add_csv():
         data = csv.reader(csv_file)
         # Modified from code at: https://linuxhint.com/skip-header-row-csv-python/
         next(data)
+        
+        
         for row in data:
-            item_in_db = session.query(Inventory).filter(Inventory.product_name==row[0]).one_or_none()
-            compare_items(item_in_db, row)
+            item_list = []
+            for item in session.query(Inventory).with_entities(Inventory.product_name).all():
+                item_list.append(item.product_name)
+            if row[0] not in item_list:
+                name = row[0]
+                price = clean_price(row[1])
+                qty = int(row[2])
+                date = clean_date(row[3])
+                new_item = Inventory(
+                    product_name=name, product_price=price, product_qty=qty, date_updated=date)
+                session.add(new_item)
+                session.commit()
+                logging.info(f'New item added to database: {new_item}.')
+            elif row[0] in item_list:
+                db_item = session.query(Inventory).filter(Inventory.product_name==row[0]).first()
+                if db_item.date_updated > clean_date(row[3]):
+                    logging.info(
+                        f'"{db_item.product_name}" already exists in the database and a more recent item exists in the database. Discarding item.')
+                elif db_item.date_updated < clean_date(row[3]):
+                    logging.info(f'***** REPLACING Item *****')
+                    logging.info(
+                        f'\tOld Item: Name: {db_item.product_name}, ID: {db_item.product_id}, Price: {db_item.product_price}, Quantity: {db_item.product_qty}, Date Updated: {db_item.date_updated}.')
+                    logging.info(
+                        f'\tNew Item: Name: {row[0]}, ID: {db_item.product_id}, Price: {clean_price(row[1])}, Quantity: {int(row[2])}, Date Updated: {clean_date(row[3])}.')
+                    db_item.product_name = row[0]
+                    db_item.product_price = clean_price(row[1])
+                    db_item.product_qty = int(row[2])
+                    db_item.date_updated = clean_date(row[3])
+                    session.commit()
+                    logging.info(
+                        f'{db_item.product_name} successfully updated.')
+                else:
+                    logging.info(
+                        f'"{db_item.product_name}" already exists in the database and is the latest entry. Discarding item.')
 
 def menu():
     while True:
@@ -192,5 +273,6 @@ def app():
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    #add_csv()
-    app()
+    # add_csv()
+    # app()
+
