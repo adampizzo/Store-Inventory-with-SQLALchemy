65eeee3ecc6f6be784ee9836e9f0679406cd7f50
adampizzo <Adampizzo@icloud.com>
2022-04-09 22:27:11 -0400

    Added menu functions
    Added display_inventory and search_inventory_by_id functions, as well as the 'x' feature in menu.

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ logging.basicConfig(filename='log\store.log', encoding='utf-8', level=logging.DE
                     format='%(asctime)s %(levelname)s %(message)s', datefmt='%m-%d-%Y %H:%M UTC')
 logging.Formatter.converter = time.gmtime # From https://docs.python.org/3/library/logging.html
 
+class NotInListError(Exception):
+    pass
 
 def clean_price(price_str):
     try:
@@ -45,6 +47,31 @@ def clean_date(date_str):
         return return_date
 
 
+def clean_id(id_str, id_options):
+    try:
+        item_id = int(id_str)
+        if item_id not in id_options:
+            raise NotInListError
+    except ValueError:
+        input('''
+            \n****** ID ERROR ******
+            \rThe ID should be a number.
+            \rEx: 10
+            \rPress enter to try again.
+            \r*************************''')
+        return
+    except NotInListError:
+        input(f'''
+            \n****** ID ERROR ******
+            \rThe ID should be one that is in the following list.
+            \rOptions: {id_options}
+            \rPress enter to try again.
+            \r*************************''')
+        return
+    else:
+        return item_id
+
+
 def compare_items(db_item, other_item):
     if db_item == None:
         name = other_item[0]
@@ -71,7 +98,43 @@ def compare_items(db_item, other_item):
             f'{db_item.product_name} successfully updated.')
     else:
         logging.info(f'"{db_item.product_name}" already exists in the database and no other entries detected. Discarding item.')
-    
+
+
+def display_inventory():
+    print()
+    for item in session.query(Inventory):
+        print(f'{item.product_id} | {item.product_name}')
+        time.sleep(.05)
+    input('\nPress enter to return to the main menu...')
+
+
+def search_inventory_by_id():
+    id_list = []
+    for item in session.query(Inventory):
+        id_list.append(item.product_id)
+    id_error = True
+    while id_error:
+        print(f'The IDs of items in the store are:\n{id_list}\n')
+        id_choice = input('Please enter the ID of an item: ')
+        id_choice = clean_id(id_choice, id_list)
+        if type(id_choice) == int:
+            id_error = False
+    the_item = session.query(Inventory).filter(Inventory.product_id == id_choice).first()
+    print(f'''
+        \n{the_item.product_name} -
+        \rPrice: {the_item.product_price/100}
+        \rQuantity: {the_item.product_qty}
+        \rLast Updated: {the_item.date_updated.strftime("%m-%d-%Y")}\n''')
+    input('Press enter to return to the main menu...')
+
+
+def add_product_to_inventory():
+    pass
+
+
+def backup_inventory():
+    pass
+
 
 def add_csv():
     with open('store-inventory\inventory.csv') as csv_file:
@@ -84,23 +147,50 @@ def add_csv():
 
 def menu():
     while True:
+        valid_choices = ['p', 'v', 'a', 'b', 'x']
         print('''
             \n***** STORE INVENTORY SYSTEM *****
-            \r1) Add Book
-            \r2) View all books
-            \r3) Search for book by ID
-            \r4) Book Analysis
-            \r5) Exit''')
+            \rp) Print all items in the database
+            \rv) View a single product's inventory
+            \ra) Add a new product to the database
+            \rb) Make a backup of the entire inventory
+            \rx) Exit''')
         choice = input('What would you like to do? ')
-        if choice in ['a', 'v', 'b']:
+        if choice in valid_choices:
             return choice
         else:
-            input('''
-                \rPlease choose of the options above.
-                \rA number from 1-5.
+            input(f'''
+                \rPlease only choose items from the following list:
+                \r{valid_choices}
                 \rPress enter to try again.''')
+
+
+def app():
+    app_running = True
+    while app_running:
+        user_choice = menu()
+        if user_choice == 'p':
+            # "View all items in the database"
+            display_inventory()
+
+        elif user_choice == 'v':
+            # "View a single product's inventory"
+            search_inventory_by_id()
+            
+        elif user_choice == 'a':
+            # "Add a new product to the database"
+            pass
+        elif user_choice == 'b':
+            # "Make a backup of the entire inventory"
+            pass
+        elif user_choice == 'x':
+            # "Exit Program"
+            print("Thank you for utilizing the store inventory system. Have a good day!")
+            time.sleep(2)
+            app_running = False
 
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    add_csv()
+    #add_csv()
+    app()
